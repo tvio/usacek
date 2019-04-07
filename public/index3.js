a3f7032c4b9290ed6824fcafe571fe1a77571063
tvio <tvio@centrum.cz>
2019-04-07 22:47:43 +0200

    fung+format sloup

--- public/index3.js
+++ public/index3.js
@@ -1,21 +1,36 @@
 
+
 $(document).ready(function() {
-  
+     
+    
     $('#primetable').DataTable( {
+        filter: true,
+        order: [1,'asc'],
         processing: true,
-        serverSide: true,
+        serverSide: false,
         ajax:{url:"http://localhost:3000/usacek",dataSrc:""},
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         type: "GET",
         columns: [
            
-            { data: "kdo" },
-            { data: "datum" },
-            { data: "pozn1" },
-            { data: "pozn2" },
+            { data: "kdo" ,
+             render: function (val,type,row){
+                 return val==1 ?"Janik":"Dalibor"
+                 
+             }},
+            { data: "datum",
+                "render": function (val, type, row) {
+                    return moment(val).format('DD.MM.YYYY')
+                }},
+            { data: "pozn1" , label :"Janik pozn"},
+            { data: "pozn2" , label : "Dalibor pozn"},
            
+        ],
+        fields: [
+         
         ]
+       
     } );
 } );
     
