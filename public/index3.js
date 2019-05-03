f9e1a8887465e00970f9323fe5b86e083419dc6c
tvio <tvio@centrum.cz>
2019-05-03 23:07:25 +0200

    popup

--- public/index3.js
+++ public/index3.js
@@ -1,74 +1,8 @@
 
-
-// $(document).ready(function() {
-     
-    
-//     $('#primetable').DataTable( {
-//         filter: true,
-//        order: [1,'asc'],
-//         processing: true,
-//         serverSide: false,
-//         ajax:{url:"http://localhost:3000/usacek",dataSrc:""},
-//         dataType: 'json',
-//         contentType: 'application/json; charset=utf-8',
-//         type: "GET",
-//         columns: [
-           
-//             { data: "kdo" ,
-//              render: function (val,type,row){
-//                  return val==1 ?"Janik":"Dalibor"
-                 
-//              }},
-//             { data: "datum",
-//                 "render": function (val, type, row) {
-//                     return moment(val).format('DD.MM.YYYY')
-//                 }},
-//             { data: "pozn1" , name :"Janik pozn"},
-//             { data: "pozn2" , name : "Dalibor pozn"},
-           
-//         ],   
-       
-//     } );
-    var editor; // use a global for the submit and return data rendering in the examples
- 
     $(document).ready(function() {
-        editor = new $.fn.dataTable.Editor( {
-            ajax: {
-                create: {
-                    type: 'POST',
-                    url:  'http://localhost:3000/usacek'
-                },
-                edit: {
-                    type: 'PUT',
-                    url:  'http://localhost:3000/usacek/_id_'
-                },
-                remove: {
-                    type: 'DELETE',
-                    url:  'http://localhost:3000/usacek/_id_'
-                }
-            },
-            table: "#primetable",
-            idSrc: 'id',
-            fields: [ {
-                    label: "Kdo",
-                    name: "kdo"
-                }, {
-                    label: "Datum",
-                    name: "datum",
-                    
-                }, {
-                    label: "Poznámka Janík:",
-                    name: "pozn1"
-                }, {
-                    label: "Poznámka Dalibor:",
-                    name: "pozn2"
-                }
-            ]
-        } );
-     
-        $('#primetable').DataTable( {
-            dom: 'Bfrtip',
-            filter: true,
+        
+            var table = $('#primetable').DataTable( {
+           filter: true,
             order: [1,'asc'],
             processing: true,
             serverSide: false,
@@ -87,14 +21,37 @@
                 { data: "pozn2" },
              
             ],
-            select: true,
-            buttons: [
-                { extend: "create", editor: editor },
-                { extend: "edit",   editor: editor },
-                { extend: "remove", editor: editor }
-            ]
+            select: true           
         } );
-    } );
-  
+ 
+ 
+        $('#primetable tbody').on( 'click', 'tr', function () {
+   console.log( table.row( this ).data() );
+            data = table.row( this ).data();
+   for (var i in data) {
+       console.log(data[i]+','+i);
+       if (i=="datum")  {
+           console.log("dostanu se na datum")
+        $('input[id="datum"]').val(moment(data[i]).format('DD.MM.YYYY'));
+       } else if
+        (i=="kdo") {
+            $('input[id="kdo"]').val(data[i]==1 ?"Janik":"Dalibor");
+        }
+           else
+    $('textarea[id="'+i+'"]').val(data[i]);
+   }
+    
+      
+
+    $('#modal').modal('show'); 
+     });
+
+    
+     
+    });
+ 
+          
+
+         
 
     
