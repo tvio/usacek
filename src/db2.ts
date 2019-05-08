109852df34e999d39ebf260582099175b0b1c5d0
tvio <tvio@centrum.cz>
2019-05-08 23:01:35 +0200

    uprava connect sttringu na DB PG v heroku

--- src/db2.ts
+++ src/db2.ts
@@ -4,15 +4,21 @@ import moment  from 'moment';
 const {Client} = require ('pg');
 
 
-const config = {
-    user: 'test', // env var: PGUSER
-    database: 'test', // env var: PGDATABASE
-    password: 'test', // env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
-  }
+// const config = {
+//     user: 'test', // env var: PGUSER
+//     database: 'test', // env var: PGDATABASE
+//     password: 'test', // env var: PGPASSWORD
+//     host: 'localhost', // Server hosting the postgres database
+//     port: 5432, // env var: PGPORT
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+//   }
+
+const config = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 
 //export async function selectById
 
